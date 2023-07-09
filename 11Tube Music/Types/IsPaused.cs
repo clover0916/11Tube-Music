aea2107304d39d69027e8e8635592e6e6d6aae28
Clover_Midori <youhuid05@gmail.com>
2023-07-09 15:58:13 +0900

    Add type

---
+++ 11Tube Music/Types/IsPaused.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenTube_Music.Types
+{
+    public class IsPaused
+    {
+        public bool paused { get; set; }
+        public double currentTime { get; set; }
+    }
+}
