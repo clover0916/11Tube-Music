2806a873db5c16bade15e7c7adc11f7a77f8ce5d
Clover_Midori <youhuid05@gmail.com>
2023-07-23 16:22:57 +0900

    Add types

---
+++ 11Tube Music/Types/Playlists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenTube_Music.Types
+{
+    public class Playlist
+    {
+        public string title { get; set; }
+        public string subtitle { get; set; }
+    }
+}
--- 11Tube Music/Types/PluginConfig.cs
+++ 11Tube Music/Types/PluginConfig.cs
@@ -12,6 +12,9 @@ namespace ElevenTube_Music.Settings.Types
         public string description { get; set; }
         public string version { get; set; }
         public Author author { get; set; }
+        #nullable enable
+        public Option[]? option { get; set; }
+        #nullable disable
     }
 
     public class Author
@@ -24,5 +27,15 @@ namespace ElevenTube_Music.Settings.Types
     {
         public string name { get; set; }
         public string url { get; set; }
+    }
+
+    public class Option
+    {
+        public string type { get; set; }
+        public string name { get; set; }
+        #nullable enable
+        public string? display_name { get; set; }
+        #nullable disable
+        public string description { get; set; }
     }
 }
